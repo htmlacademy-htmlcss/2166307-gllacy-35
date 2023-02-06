2a60c084dabaf3ae52c3d2add5234cbfa3c68365
AndrewPlatonov888 <very@blacky.ru>
2023-02-06 16:30:15 +0300

    adds click&ESC closure, filter draft

--- scripts/nav-master.js
+++ scripts/nav-master.js
@@ -12,6 +12,61 @@ var BasketEvent = document.getElementById('basket-btn');
 BasketEvent.addEventListener('click', BasketToggleFunction);
 
 
+
+// закрытие меню по нажатию клавиши ESC
+document.addEventListener('keydown', function(e) {
+	if( e.keyCode == 27 ){ // код клавиши Escape, но можно использовать e.key
+		 //закрываем логин, если он открыт
+     document.querySelector('.login-form').style.setProperty("--display", "none");
+     document.getElementById("login-btn").value="off";
+     document.querySelector('.login-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
+     document.querySelector('.login-button').style.setProperty("--background-image", "url('../images/login-icon.svg')");
+     document.querySelector('.login-button').style.setProperty("--color", "#2D3440");
+     document.querySelector('.login-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
+     document.querySelector('.login-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
+     document.querySelector('.login-button').style.setProperty("--opacity-hover", "1");
+     document.querySelector('.login-button').style.setProperty("--opacity-active", "1");
+     document.querySelector('.login-button').style.setProperty("--border", "2px solid #2D3440");
+
+     //закрываем корзину, если она открыта
+   document.querySelector('.basket-form').style.setProperty("--display", "none");
+   document.getElementById("basket-btn").value="off";
+   document.querySelector('.basket-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
+   document.querySelector('.basket-button').style.setProperty("--background-image", "url('../images/сart-icon.svg')");
+   document.querySelector('.basket-button').style.setProperty("--color", "#2D3440");
+   document.querySelector('.basket-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
+   document.querySelector('.basket-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
+   document.querySelector('.basket-button').style.setProperty("--opacity-hover", "1");
+   document.querySelector('.basket-button').style.setProperty("--opacity-active", "1");
+   document.querySelector('.basket-button').style.setProperty("--border", "2px solid #2D3440");
+
+   //закрываем поиск, если он открыт
+document.querySelector('.search-form').style.setProperty("--display", "none");
+document.getElementById("search-btn").value="off";
+ document.querySelector('.search-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
+ document.querySelector('.search-button').style.setProperty("--background-image", "url('../images/search-icon.svg')");
+ document.querySelector('.search-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
+ document.querySelector('.search-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
+ document.querySelector('.search-button').style.setProperty("--opacity-hover", "1");
+ document.querySelector('.search-button').style.setProperty("--opacity-active", "1");
+ document.querySelector('.search-button').style.setProperty("--border", "2px solid #2D3440");
+
+//закрываем каталог-меню, если оно открыто
+ document.querySelector('.catalog-menu').style.setProperty("--display", "none");
+document.getElementById("catalog-btn").value="off";
+document.querySelector('.catalog-button').style.setProperty("--background-color", "transparent");
+ document.querySelector('.catalog-button').style.setProperty("--color", "#2D3440");
+ document.querySelector('.catalog-button').style.setProperty("--top", "8.5px");
+ document.querySelector('.catalog-button').style.setProperty("--background-image", "url('../images/down-arrow.svg')");
+ document.querySelector('.catalog-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
+ document.querySelector('.catalog-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
+ document.querySelector('.catalog-button').style.setProperty("--opacity-hover", "1");
+ document.querySelector('.catalog-button').style.setProperty("--opacity-active", "1");
+ document.querySelector('.catalog-button').style.setProperty("--border", "2px solid #2D3440");
+	}
+});
+
+
 function CatalogToggleFunction() {
   if(document.getElementById("catalog-btn").value=="off") {
    document.getElementById("catalog-btn").value="on";
@@ -27,8 +82,31 @@ function CatalogToggleFunction() {
  document.querySelector('.catalog-button').style.setProperty("--border", "2px solid #000000"); //сделали фокусу ЧЕРНЫЙ бордер
  document.querySelector('.catalog-menu').style.setProperty("--display", "flex");
 
+ // закрытие элемента по клику вне элемента
+ document.addEventListener("mouseup", function(event) {
+  var obj = document.querySelector('.catalog-menu');
+  if (!obj.contains(event.target) ) {
+    document.querySelector('.catalog-menu').style.setProperty("--display", "none");
+    document.getElementById("catalog-btn").value="off";
+    document.querySelector('.catalog-button').style.setProperty("--background-color", "transparent");
+     document.querySelector('.catalog-button').style.setProperty("--color", "#2D3440");
+     document.querySelector('.catalog-button').style.setProperty("--top", "8.5px");
+     document.querySelector('.catalog-button').style.setProperty("--background-image", "url('../images/down-arrow.svg')");
+     document.querySelector('.catalog-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
+     document.querySelector('.catalog-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
+     document.querySelector('.catalog-button').style.setProperty("--opacity-hover", "1");
+     document.querySelector('.catalog-button').style.setProperty("--opacity-active", "1");
+     document.querySelector('.catalog-button').style.setProperty("--border", "2px solid #2D3440");
+  }
+  else {
+    //  alert("Обнаружен ВНУТРЕННИЙ клик!");
+; //иначе, если клик состоялся по элементу ---> тогда НИЧЕГО не делаем
+  }
+} );
+
 }
 else {
+
 document.querySelector('.catalog-menu').style.setProperty("--display", "none");
 document.getElementById("catalog-btn").value="off";
 //все св-ва возвращаем на исходные позиции
@@ -58,29 +136,26 @@ function SearchToggleFunction() {
    document.querySelector('.search-button').style.setProperty("--border", "2px solid #000000"); //сделали фокусу ЧЕРНЫЙ бордер
    document.querySelector('.search-form').style.setProperty("--display", "flex");
 
-     //закрываем логин, если он открыт
-  document.querySelector('.login-form').style.setProperty("--display", "none");
-  document.getElementById("login-btn").value="off";
-  document.querySelector('.login-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
-  document.querySelector('.login-button').style.setProperty("--background-image", "url('../images/login-icon.svg')");
-  document.querySelector('.login-button').style.setProperty("--color", "#2D3440");
-  document.querySelector('.login-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
-  document.querySelector('.login-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
-  document.querySelector('.login-button').style.setProperty("--opacity-hover", "1");
-  document.querySelector('.login-button').style.setProperty("--opacity-active", "1");
-  document.querySelector('.login-button').style.setProperty("--border", "2px solid #2D3440");
-
-  //закрываем корзину, если она открыта
-document.querySelector('.basket-form').style.setProperty("--display", "none");
-document.getElementById("basket-btn").value="off";
-document.querySelector('.basket-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
-document.querySelector('.basket-button').style.setProperty("--background-image", "url('../images/сart-icon.svg')");
-document.querySelector('.basket-button').style.setProperty("--color", "#2D3440");
-document.querySelector('.basket-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
-document.querySelector('.basket-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
-document.querySelector('.basket-button').style.setProperty("--opacity-hover", "1");
-document.querySelector('.basket-button').style.setProperty("--opacity-active", "1");
-document.querySelector('.basket-button').style.setProperty("--border", "2px solid #2D3440");
+   // закрытие элемента по клику вне элемента
+ document.addEventListener("mouseup", function(event) {
+  var obj = document.querySelector('.search-form');
+  if (!obj.contains(event.target) ) {
+    document.querySelector('.search-form').style.setProperty("--display", "none");
+document.getElementById("search-btn").value="off";
+//все св-ва возвращаем на исходные позиции
+ document.querySelector('.search-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
+ document.querySelector('.search-button').style.setProperty("--background-image", "url('../images/search-icon.svg')");
+ document.querySelector('.search-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
+ document.querySelector('.search-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
+ document.querySelector('.search-button').style.setProperty("--opacity-hover", "1");
+ document.querySelector('.search-button').style.setProperty("--opacity-active", "1");
+ document.querySelector('.search-button').style.setProperty("--border", "2px solid #2D3440");
+  }
+  else {
+    //  alert("Обнаружен ВНУТРЕННИЙ клик!");
+; //иначе, если клик состоялся по элементу ---> тогда НИЧЕГО не делаем
+  }
+} );
 
 }
 else {
@@ -114,28 +189,27 @@ function LoginToggleFunction() {
    document.querySelector('.login-button').style.setProperty("--display", "flex");
   document.querySelector('.login-form').style.setProperty("--display", "flex");
 
-//закрываем корзину, если она открыта
-document.querySelector('.basket-form').style.setProperty("--display", "none");
-document.getElementById("basket-btn").value="off";
-document.querySelector('.basket-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
-document.querySelector('.basket-button').style.setProperty("--background-image", "url('../images/сart-icon.svg')");
-document.querySelector('.basket-button').style.setProperty("--color", "#2D3440");
-document.querySelector('.basket-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
-document.querySelector('.basket-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
-document.querySelector('.basket-button').style.setProperty("--opacity-hover", "1");
-document.querySelector('.basket-button').style.setProperty("--opacity-active", "1");
-document.querySelector('.basket-button').style.setProperty("--border", "2px solid #2D3440");
-
-//закрываем поиск, если он открыт
-document.querySelector('.search-form').style.setProperty("--display", "none");
-document.getElementById("search-btn").value="off";
- document.querySelector('.search-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
- document.querySelector('.search-button').style.setProperty("--background-image", "url('../images/search-icon.svg')");
- document.querySelector('.search-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
- document.querySelector('.search-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
- document.querySelector('.search-button').style.setProperty("--opacity-hover", "1");
- document.querySelector('.search-button').style.setProperty("--opacity-active", "1");
- document.querySelector('.search-button').style.setProperty("--border", "2px solid #2D3440");
+   // закрытие элемента по клику вне элемента
+   document.addEventListener("mouseup", function(event) {
+    var obj = document.querySelector('.login-form');
+    if (!obj.contains(event.target) ) {
+      document.querySelector('.login-form').style.setProperty("--display", "none");
+document.getElementById("login-btn").value="off";
+//все св-ва возвращаем на исходные позиции
+document.querySelector('.login-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
+document.querySelector('.login-button').style.setProperty("--background-image", "url('../images/login-icon.svg')");
+document.querySelector('.login-button').style.setProperty("--color", "#2D3440");
+document.querySelector('.login-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
+document.querySelector('.login-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
+document.querySelector('.login-button').style.setProperty("--opacity-hover", "1");
+document.querySelector('.login-button').style.setProperty("--opacity-active", "1");
+document.querySelector('.login-button').style.setProperty("--border", "2px solid #2D3440");
+    }
+    else {
+      //  alert("Обнаружен ВНУТРЕННИЙ клик!");
+  ; //иначе, если клик состоялся по элементу ---> тогда НИЧЕГО не делаем
+    }
+  } );
 }
 else {
 document.querySelector('.login-form').style.setProperty("--display", "none");
@@ -171,29 +245,28 @@ function BasketToggleFunction() {
   document.querySelector('.basket-button').style.setProperty("--display", "flex");
   document.querySelector('.basket-form').style.setProperty("--display", "flex");
 
-  //закрываем логин, если он открыт
-  document.querySelector('.login-form').style.setProperty("--display", "none");
-document.getElementById("login-btn").value="off";
-document.querySelector('.login-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
-document.querySelector('.login-button').style.setProperty("--background-image", "url('../images/login-icon.svg')");
-document.querySelector('.login-button').style.setProperty("--color", "#2D3440");
-document.querySelector('.login-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
-document.querySelector('.login-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
-document.querySelector('.login-button').style.setProperty("--opacity-hover", "1");
-document.querySelector('.login-button').style.setProperty("--opacity-active", "1");
-document.querySelector('.login-button').style.setProperty("--border", "2px solid #2D3440");
+  // закрытие элемента по клику вне элемента
+  document.addEventListener("mouseup", function(event) {
+    var obj = document.querySelector('.basket-form');
+    if (!obj.contains(event.target) ) {
+      document.querySelector('.basket-form').style.setProperty("--display", "none");
+      document.getElementById("basket-btn").value="off";
 
-//закрываем поиск, если он открыт
-document.querySelector('.search-form').style.setProperty("--display", "none");
-document.getElementById("search-btn").value="off";
- document.querySelector('.search-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
- document.querySelector('.search-button').style.setProperty("--background-image", "url('../images/search-icon.svg')");
- document.querySelector('.search-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
- document.querySelector('.search-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
- document.querySelector('.search-button').style.setProperty("--opacity-hover", "1");
- document.querySelector('.search-button').style.setProperty("--opacity-active", "1");
- document.querySelector('.search-button').style.setProperty("--border", "2px solid #2D3440");
-
+      //все св-ва возвращаем на исходные позиции
+      document.querySelector('.basket-button').style.setProperty("--background-color", "rgba(252, 252, 252, 0.3)");
+      document.querySelector('.basket-button').style.setProperty("--background-image", "url('../images/сart-icon.svg')");
+      document.querySelector('.basket-button').style.setProperty("--color", "#2D3440");
+      document.querySelector('.basket-button').style.setProperty("--background-hover", "rgba(252, 252, 252, 0.5)");
+      document.querySelector('.basket-button').style.setProperty("--background-active", " rgba(252, 252, 252, 1)");
+      document.querySelector('.basket-button').style.setProperty("--opacity-hover", "1");
+      document.querySelector('.basket-button').style.setProperty("--opacity-active", "1");
+      document.querySelector('.basket-button').style.setProperty("--border", "2px solid #2D3440");
+    }
+    else {
+      //  alert("Обнаружен ВНУТРЕННИЙ клик!");
+  ; //иначе, если клик состоялся по элементу ---> тогда НИЧЕГО не делаем
+    }
+  } );
 }
 else {
 document.querySelector('.basket-form').style.setProperty("--display", "none");
